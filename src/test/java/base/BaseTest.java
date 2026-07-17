d53f7143a227a33709db5fad8b809dedba8c178d
Anil Samal <samalanil627@gmail.com>
2026-07-17 18:55:41 +0530

    Lesson 6: Added Playwright tracing support

--- src/test/java/base/BaseTest.java
+++ src/test/java/base/BaseTest.java
@@ -1,16 +1,19 @@
 package base;
 
+import java.io.File;
+import java.nio.file.Paths;
+
 import org.apache.logging.log4j.Logger;
-
-import utils.LoggerUtil;
-
+import org.testng.ITestResult;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
 
 import com.microsoft.playwright.Page;
+import com.microsoft.playwright.Tracing;
 
 import factory.PlaywrightFactory;
 import utils.ConfigReader;
+import utils.LoggerUtil;
 
 public class BaseTest {
 
@@ -36,6 +39,12 @@ public class BaseTest {
         factory = new PlaywrightFactory();
 
         page = factory.initBrowser(config.getBrowser(), config.isHeadless());
+        factory.getContext().tracing().start(
+        	    new Tracing.StartOptions()
+        	        .setScreenshots(true)
+        	        .setSnapshots(true)
+        	        .setSources(true)
+        	);
         currentPage = page;
 
         log.info("Navigating to URL: {}", config.getUrl());
@@ -50,14 +59,33 @@ public class BaseTest {
     }
 
     @AfterMethod(alwaysRun = true)
-    public void tearDown() {
+    public void tearDown(ITestResult result) {
 
-        log.info("Closing browser");
+        try {
+            if (result.getStatus() == ITestResult.FAILURE) {
 
-        if (factory != null) {
-            factory.closeBrowser();
+                new File("traces").mkdirs();
+
+                factory.getContext().tracing().stop(
+                    new Tracing.StopOptions()
+                        .setPath(Paths.get(
+                            "traces/"
+                            + result.getMethod().getMethodName()
+                            + "_"
+                            + System.currentTimeMillis()
+                            + ".zip"))
+                );
+
+            } else {
+
+                factory.getContext().tracing().stop();
+            }
+
+        } finally {
+
+            if (factory != null) {
+                factory.closeBrowser();
+            }
         }
-
-        log.info("Browser closed");
     }
 }
